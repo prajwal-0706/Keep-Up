c8d76f75d1f80eb078d969308a1df2ff549c5e6d
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-11-30 21:42:00 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,15 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
+      <Button>
         <Image
-          src="/logo.png"
+          src="/logo.svg"
           alt="Keep Up"
-          width={128}
-          height={128}
+          width={200}
+          height={200}
           className="rounded-full"
         />
-        <h1 className="mt-8 text-4xl font-bold text-gray-900">Keep Up</h1>
-        <p className="mt-4 text-gray-600">
-          This Project is used to keep up the notes like a diary or a notion app
-        </p>
-      </div>
+      </Button>
     </main>
   );
 }
