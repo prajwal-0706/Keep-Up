e7aa8e9c375456acc863e89e8d92fe46feeb6e14
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-12-02 21:42:59 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,12 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        <Image
-          src="/logo.svg"
-          alt="Keep Up"
-          width={200}
-          height={200}
-          className="rounded-full"
-        />
-      </Button>
+      <h1
+        className="text-4xl font-bold text-center text-gray-900"
+        style={{ lineHeight: 1.25 }}
+      >
+        Hello World
+      </h1>
     </main>
   );
 }
