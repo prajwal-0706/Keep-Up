84e36e7ae5ef2b2cbfaa8cfe9be0b831b70a786a
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-12-04 22:23:16 +0530

    Fixed some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       ) : (
         <Button
           onClick={() => {
-            setIsloading(true);
+            setIsloading((prev) => !prev);
           }}
         >
           <Mail className="mr-2 h-4 w-4" /> Login with Email
