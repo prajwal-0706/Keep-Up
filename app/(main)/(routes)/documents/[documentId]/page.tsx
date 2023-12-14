14df01379d5a16735809b884a8144bf1b75d0d71
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-12-15 00:52:08 +0530

    Added toolbar for adding adding icon and emoji inside the note

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+'use client';
 
-export default function Page() {
-  return <div>Page</div>;
+import Toolbar from '@/components/Toolbar';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { useQuery } from 'convex/react';
+
+interface DocumentPageProps {
+  params: {
+    documentId: Id<'documents'>;
+  };
+}
+
+export default function Page({ params }: DocumentPageProps) {
+  const documents = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (documents === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (documents === null) {
+    return null;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={documents} />
+      </div>
+    </div>
+  );
 }
