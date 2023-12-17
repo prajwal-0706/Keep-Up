0fa72abd28188aae9d9860d6320f6dc5a74a6de4
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-12-17 14:58:05 +0530

    Added publishing functionality

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -41,8 +41,11 @@ export default function Page({ params }: DocumentPageProps) {
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
             <Skeleton className="h-14 w-[50%]" />
+
             <Skeleton className="h-4 w-[80%]" />
+
             <Skeleton className="h-4 w-[40%]" />
+
             <Skeleton className="h-4 w-[60%]" />
           </div>
         </div>
