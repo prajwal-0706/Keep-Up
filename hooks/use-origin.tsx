f3ed4ca066dda3fa49dff99b1cacb99aa3527f7c
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-02-22 18:49:32 +0530

    fix: Some of the Imports fixed

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useOrigin() {
   const [mounted, setMounted] = useState(false);
--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useScrollTop(threshold = 10) {
   const [scrolled, setScrolled] = useState(false);
